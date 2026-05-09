ed3ca435d5b8095dadd1cf555f787272cb2e31fc
aebonlee <aebon@kyonggi.ac.kr>
2026-05-09 23:38:13 +0900

    Updates

--- assets/aiService-DftU_HPl.js
+++ assets/aiService-DftU_HPl.js
@@ -1,4 +1,4 @@
-import{g as p}from"./colorSchemes-ucW8M7Qu.js";import{g as c}from"./index-DGx7O8ab.js";const g=`{
+import{g as p}from"./colorSchemes-ucW8M7Qu.js";import{g as c}from"./index-DvJfsr4_.js";const g=`{
   "title": "string - presentation title",
   "description": "string - short description",
   "slides": [
